a03dbfe618ff7d48fc814065799ac902b4afd547
DKevinM <35807537+DKevinM@users.noreply.github.com>
2026-01-17 20:52:09 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const openStreetMapLayer = L.tileLayer(
 // ---------- SINGLE MAP ----------
 const map = L.map('map', {
   layers: [openStreetMapLayer]
-}).setView([51.045150, -114.045313], 12);
+}).setView([51.045150, -114.045313], 14);
 
 // ---------- ONE SET OF LAYER GROUPS ONLY ----------
 const markerGroup = L.layerGroup().addTo(map);
