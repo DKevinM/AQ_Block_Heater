f424dcfccf993874e5c9be25e3e64c9c31e8d4ed
DKevinM <35807537+DKevinM@users.noreply.github.com>
2026-01-22 12:57:59 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,13 +2,21 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('aqhi-cache').then(cache => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/js/main.js',
-        '/js/purpleair.js',
-        '/js/click_engine.js',
-        '/history/sensor_compare.html'
+        './',
+        './index.html',
+        './js/main.js',
+        './js/purpleair.js',
+        './js/click_engine.js',
+        './history/sensor_compare.html'
       ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
     })
   );
 });
